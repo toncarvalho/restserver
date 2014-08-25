19081292638f365fb1a67967c93d120dd3132631
Ton Carvalho <wellington.carvalho@atualcs.com.br>
2014-08-25 15:56:29 -0300

    testes

--- src/main/java/restserver/services/ServerStatusRest.java
+++ src/main/java/restserver/services/ServerStatusRest.java
@@ -12,6 +12,7 @@ import javax.ws.rs.core.Response;
 public class ServerStatusRest {
     @GET
     public Response status() {
+
         return Response.status(Response.Status.OK).build();
     }
 }
